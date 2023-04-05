ca5b5156320149eed9930db1e16f57db8905353b
andiharun29 <andiharunalrasyid14@gmail.com>
2023-04-05 10:44:22 +0700

    membuat structure

--- ACTIVITY5/ACTIVITY5.cpp
+++ ACTIVITY5/ACTIVITY5.cpp
@@ -9,18 +9,3 @@ struct mahasiswa
 	int umur;
 };
 
-int main() {
-	mahasiswa mhs, mhs2;
-
-	mhs.nim = "20220140099";
-	mhs.umur = 19;
-	mhs.nama = "andi harun al rasyid";
-	mhs.alamat = "kendari";
-
-	cout << "masukkan NIM : ";
-	cin >> mhs2.nim;
-	cout << "masukkan umur : ";
-	cin >> mhs2.umur;
-	cout << "masukkan alamat : ";
-
-}
